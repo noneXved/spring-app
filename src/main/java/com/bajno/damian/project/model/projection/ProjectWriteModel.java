affe3f1ce2a60e8cbfc95bdec3d555bdd590cf77
noneXved <bajno.damian96@gmail.com>
2021-06-05 23:04:12 +0200

    Implement DTO ProjectWriteModel

---
+++ src/main/java/com/bajno/damian/project/model/projection/ProjectWriteModel.java
@@ -0,0 +1,42 @@
+package com.bajno.damian.project.model.projection;
+
+import com.bajno.damian.project.model.Project;
+import com.bajno.damian.project.model.ProjectStep;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.List;
+
+public class ProjectWriteModel {
+    @NotBlank(message = "Project's description must not be empty")
+    private String description;
+
+    @Valid
+    private List<ProjectStep> steps;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<ProjectStep> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(List<ProjectStep> steps) {
+        this.steps = steps;
+    }
+
+    public Project toProject() {
+        var result = new Project();
+        result.setDescription(description);
+        steps.forEach(step -> step.setProject(result));
+        result.setSteps(new HashSet<>(steps));
+
+        return result;
+    }
+}
